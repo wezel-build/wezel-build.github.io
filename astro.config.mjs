5bd7c77deccb5b0d34a78cb9aafec0bccfc42a80
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2026-03-03 10:10:50 +0100

    Update slogan

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,5 @@
-import { defineConfig } from 'astro/config';
-export default defineConfig({});
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://wezel-dev.github.io",
+});
